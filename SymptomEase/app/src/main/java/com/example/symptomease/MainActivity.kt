26daf39e8046beaca362853e5119021cea90a2ec
Trivon-Paul <95836132+Trivon-Paul@users.noreply.github.com>
2023-04-13 16:00:44 -0400

    This is a test Trivon

--- SymptomEase/app/src/main/java/com/example/symptomease/MainActivity.kt
+++ SymptomEase/app/src/main/java/com/example/symptomease/MainActivity.kt
@@ -32,4 +32,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+    //HERE
 }
