07a6efe0305d8ab8c93246493496c725bdbfca76
Trivon-Paul <95836132+Trivon-Paul@users.noreply.github.com>
2023-04-19 13:56:04 -0400

    Created a onselectListener for spinner and send button.

--- SymptomEase/app/src/main/java/com/example/symptomease/ui/dashboard/DashboardFragment.kt
+++ SymptomEase/app/src/main/java/com/example/symptomease/ui/dashboard/DashboardFragment.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.symptomease.R
 import com.example.symptomease.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
+    private lateinit var spinnerValue : String
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -41,9 +43,38 @@ class DashboardFragment : Fragment() {
             editor.apply()
         }
 
+        var listSymptoms = mutableListOf<String>()
+
+        listSymptoms.add("Fever")
+        listSymptoms.add("cough")
+
+        val spinner = root.findViewById<Spinner>(R.id.spinner)
+
+        val symptomsAdapter = this.context?.let {
+            ArrayAdapter<String>(it, android.R.layout.simple_spinner_dropdown_item, listSymptoms) }
+        spinner.adapter = symptomsAdapter
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                spinnerValue = listSymptoms.get(p2)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
         return root
     }
 
+    fun onSendClick(view: View){
+        if(spinnerValue == null){
+            Toast.makeText(this.context, "Select a valid symptom", Toast.LENGTH_SHORT).show()
+        } else {
+            // send to next activity diagnostics
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
